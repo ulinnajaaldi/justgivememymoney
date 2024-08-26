9a4b9c08a605f46ff1d82501cc79e27aa4c101e4
ulinnajaaldi <ulinnajaaldi24@gmail.com>
2024-08-26 16:14:37 +0700

    feat: add Account routes controller

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,9 +1,8 @@
-import accounts from "@/server/Controllers/Accounts";
 import { Hono } from "hono";
 import { HTTPException } from "hono/http-exception";
 import { handle } from "hono/vercel";
 
-export const runtime = "edge";
+import accounts from "@/server/Controllers/Accounts";
 
 const app = new Hono().basePath("/api");
 
@@ -19,5 +18,7 @@ const routes = app.route("/accounts", accounts);
 
 export const GET = handle(app);
 export const POST = handle(app);
+export const PATCH = handle(app);
+export const DELETE = handle(app);
 
 export type AppType = typeof routes;
--- server/Controllers/Accounts.ts
+++ server/Controllers/Accounts.ts
@@ -1,8 +1,9 @@
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { zValidator } from "@hono/zod-validator";
-import { eq } from "drizzle-orm";
+import { and, eq, inArray } from "drizzle-orm";
 import { Hono } from "hono";
 import { HTTPException } from "hono/http-exception";
+import * as z from "zod";
 
 import { db } from "../db";
 import { accountsTable, insertAccountsSchema } from "../schema";
@@ -28,6 +29,51 @@ const accounts = new Hono()
 
     return c.json({ data });
   })
+  .get(
+    "/:id",
+    clerkMiddleware(),
+    zValidator(
+      "param",
+      z.object({
+        id: z.string().optional(),
+      }),
+    ),
+    async (c) => {
+      const auth = getAuth(c);
+      const { id } = c.req.valid("param");
+
+      if (!auth?.userId) {
+        throw new HTTPException(401, {
+          res: c.json({ error: "Unauthorized" }, 401),
+        });
+      }
+
+      if (!id) {
+        throw new HTTPException(400, {
+          res: c.json({ error: "Id is required" }, 400),
+        });
+      }
+
+      const [data] = await db
+        .select({
+          id: accountsTable.id,
+          name: accountsTable.name,
+          userId: accountsTable.userId,
+        })
+        .from(accountsTable)
+        .where(
+          and(eq(accountsTable.userId, auth.userId), eq(accountsTable.id, id)),
+        );
+
+      if (!data) {
+        throw new HTTPException(404, {
+          res: c.json({ error: "Not found" }, 404),
+        });
+      }
+
+      return c.json({ data });
+    },
+  )
   .post(
     "/",
     clerkMiddleware(),
@@ -55,6 +101,133 @@ const accounts = new Hono()
           ...values,
         })
         .returning();
+
+      return c.json({ data });
+    },
+  )
+  .post(
+    "/bulk-delete",
+    clerkMiddleware(),
+    zValidator(
+      "json",
+      z.object({
+        ids: z.array(z.string()),
+      }),
+    ),
+    async (c) => {
+      const auth = getAuth(c);
+
+      const values = c.req.valid("json");
+
+      if (!auth?.userId) {
+        throw new HTTPException(401, {
+          res: c.json({ error: "Unauthorized" }, 401),
+        });
+      }
+
+      const data = await db
+        .delete(accountsTable)
+        .where(
+          and(
+            eq(accountsTable.userId, auth.userId),
+            inArray(accountsTable.id, values.ids),
+          ),
+        )
+        .returning({
+          id: accountsTable.id,
+        });
+
+      return c.json({ message: "Deleted successfully", data });
+    },
+  )
+  .patch(
+    "/:id",
+    clerkMiddleware(),
+    zValidator(
+      "param",
+      z.object({
+        id: z.string().optional(),
+      }),
+    ),
+    zValidator(
+      "json",
+      insertAccountsSchema.pick({
+        name: true,
+      }),
+    ),
+    async (c) => {
+      const auth = getAuth(c);
+      const { id } = c.req.valid("param");
+      const values = c.req.valid("json");
+
+      if (!auth?.userId) {
+        throw new HTTPException(401, {
+          res: c.json({ error: "Unauthorized" }, 401),
+        });
+      }
+
+      if (!id) {
+        throw new HTTPException(400, {
+          res: c.json({ error: "Id is required" }, 400),
+        });
+      }
+
+      const [data] = await db
+        .update(accountsTable)
+        .set(values)
+        .where(
+          and(eq(accountsTable.userId, auth.userId), eq(accountsTable.id, id)),
+        )
+        .returning();
+
+      if (!data) {
+        throw new HTTPException(404, {
+          res: c.json({ error: "Not found" }, 404),
+        });
+      }
+
+      return c.json({ data });
+    },
+  )
+  .delete(
+    "/:id",
+    clerkMiddleware(),
+    zValidator(
+      "param",
+      z.object({
+        id: z.string().optional(),
+      }),
+    ),
+    async (c) => {
+      const auth = getAuth(c);
+      const { id } = c.req.valid("param");
+
+      if (!auth?.userId) {
+        throw new HTTPException(401, {
+          res: c.json({ error: "Unauthorized" }, 401),
+        });
+      }
+
+      if (!id) {
+        throw new HTTPException(400, {
+          res: c.json({ error: "Id is required" }, 400),
+        });
+      }
+
+      const [data] = await db
+        .delete(accountsTable)
+        .where(
+          and(eq(accountsTable.userId, auth.userId), eq(accountsTable.id, id)),
+        )
+        .returning({
+          id: accountsTable.id,
+        });
+
+      if (!data) {
+        throw new HTTPException(404, {
+          res: c.json({ error: "Not found" }, 404),
+        });
+      }
 
       return c.json({ data });
     },
