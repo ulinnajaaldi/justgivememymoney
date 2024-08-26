4f11643ffc0a2fd4a1866b901984fe4173698ae6
ulinnajaaldi <ulinnajaaldi24@gmail.com>
2024-08-26 16:13:57 +0700

    chore: update dotenv dependency and use it!

--- server/db.ts
+++ server/db.ts
@@ -1,5 +1,9 @@
-import { neon } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
+import { config } from "dotenv";
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
+
+config({ path: ".env.local" }); // or .env.local
 
 const sql = neon(process.env.DATABASE_URL!);
+
 export const db = drizzle(sql);
