a75321e4945465846c7f85027f05cef4a4b9050f
ulinnajaaldi <ulinnajaaldi24@gmail.com>
2024-09-05 22:10:55 +0700

    feat(utils): add currency formatting functions

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,21 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+export function convertAmountToMiliUnit(amount: number) {
+  return Math.round(amount * 1000);
+}
+
+export function convertAmountFromMiliunits(amount: number) {
+  return amount / 1000;
+}
+
+export function formatCurrency(amount: number) {
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+    minimumFractionDigits: 2,
+  }).format(amount);
 }
