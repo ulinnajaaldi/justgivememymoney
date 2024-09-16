750a2b4d06b45b219f6e989c229cfa42c7cb3bea
ulinnajaaldi <ulinnajaaldi24@gmail.com>
2024-09-16 17:11:17 +0700

    feat: add FilterAccount, FilterDate, and TitlePage components

---
+++ components/common/filter-account.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import qs from "query-string";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+import { useGetAccounts } from "@/useCases/Account";
+
+const FilterAccount = () => {
+  const params = useSearchParams();
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const to = params.get("to") || undefined;
+  const from = params.get("from") || undefined;
+  const account = params.get("accountId") || "all";
+
+  const { data: dataAccount, isLoading: isLoadingAccount } = useGetAccounts();
+
+  const onChange = (value: string) => {
+    const query = {
+      accountId: value === "all" ? "" : value,
+      from,
+      to,
+    };
+
+    const url = qs.stringifyUrl(
+      {
+        url: pathname,
+        query,
+      },
+      {
+        skipEmptyString: true,
+        skipNull: true,
+      },
+    );
+
+    router.push(url);
+  };
+
+  return (
+    <Select
+      value={account}
+      onValueChange={onChange}
+      disabled={isLoadingAccount}
+    >
+      <SelectTrigger className="w-full sm:w-[180px]">
+        <SelectValue placeholder="Select Account" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value="all">All</SelectItem>
+        {dataAccount?.map((account) => (
+          <SelectItem key={account.id} value={account.id}>
+            {account.name}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
+};
+
+export default FilterAccount;
---
+++ components/common/filter-date.tsx
@@ -0,0 +1,112 @@
+"use client";
+
+import React, { useState } from "react";
+
+import { format, subDays } from "date-fns";
+import { ChevronDown } from "lucide-react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import qs from "query-string";
+import { DateRange } from "react-day-picker";
+
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverClose,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+import { formatDateRange } from "@/lib/utils";
+
+const FilterDate: React.FC<{
+  disabled?: boolean;
+}> = ({ disabled }) => {
+  const params = useSearchParams();
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const to = params.get("to") || undefined;
+  const from = params.get("from") || undefined;
+  const account = params.get("accountId") || "all";
+
+  const defaultTo = new Date();
+  const defaultFrom = subDays(defaultTo, 30);
+
+  const paramState = {
+    from: from ? new Date(from) : defaultFrom,
+    to: to ? new Date(to) : defaultTo,
+  };
+
+  const [date, setDate] = useState<DateRange | undefined>(paramState);
+
+  const pushToUrl = (dateRange: DateRange | undefined) => {
+    const query = {
+      accountId: account === "all" ? "" : account,
+      from: format(dateRange?.from || defaultFrom, "yyyy-MM-dd"),
+      to: format(dateRange?.to || defaultTo, "yyyy-MM-dd"),
+    };
+
+    const url = qs.stringifyUrl(
+      {
+        url: pathname,
+        query,
+      },
+      { skipEmptyString: true, skipNull: true },
+    );
+
+    router.push(url);
+  };
+
+  const onReset = () => {
+    setDate(undefined);
+    pushToUrl(undefined);
+  };
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          disabled={disabled}
+          className="w-full sm:w-auto"
+        >
+          <span>{formatDateRange(paramState)}</span>
+          <ChevronDown className="ml-2 h-5 w-5 text-muted-foreground" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-full" align="start">
+        <Calendar
+          initialFocus
+          mode="range"
+          defaultMonth={date?.from}
+          selected={date}
+          onSelect={setDate}
+          numberOfMonths={2}
+        />
+        <div className="grid grid-cols-2 gap-5">
+          <PopoverClose asChild>
+            <Button
+              onClick={onReset}
+              variant="outline"
+              disabled={!date?.from || !date?.to || disabled}
+            >
+              Reset
+            </Button>
+          </PopoverClose>
+          <Button
+            onClick={() => {
+              pushToUrl(date);
+            }}
+            variant="default"
+            disabled={!date?.from || !date?.to || disabled}
+          >
+            Apply
+          </Button>
+        </div>
+      </PopoverContent>
+    </Popover>
+  );
+};
+
+export default FilterDate;
