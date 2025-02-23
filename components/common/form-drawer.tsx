22e734e6e3e2f066e0e0f88b46e27ecc9b6091f9
ulinnajaaldi <ulinnajaaldi24@gmail.com>
2025-02-23 20:25:55 +0700

    feat: integrate ScrollArea into FormDrawer for improved content scrolling

--- components/common/form-drawer.tsx
+++ components/common/form-drawer.tsx
@@ -4,6 +4,8 @@ import { Drawer } from "vaul";
 
 import useDrawer from "@/hook/useDrawer";
 
+import { ScrollArea } from "../ui/scroll-area";
+
 interface FormDrawerProps {
   type: string;
   title: string;
@@ -26,21 +28,23 @@ const FormDrawer: React.FC<FormDrawerProps> = (props) => {
       <Drawer.Portal>
         <Drawer.Overlay className="fixed inset-0 z-50 bg-black/40" />
         <Drawer.Content className="fixed bottom-0 right-0 z-50 mt-24 flex h-full w-full flex-col rounded-t-[10px] bg-white outline-none lg:max-w-md lg:bg-transparent">
-          <div className="relative m-0 flex h-full flex-1 rounded-l-lg bg-white p-2 pr-4 md:m-2 md:p-4 lg:rounded-lg">
-            <div className="my-auto mr-2 h-[100px] w-2 rounded-full bg-muted" />
-            <div className="mx-auto w-full">
-              <div className="mb-4">
-                <Drawer.Title className="inline-flex w-full items-center gap-2 text-sm font-medium md:text-base">
-                  {title}
-                </Drawer.Title>
-                {description && (
-                  <Drawer.Description className="text-xs text-neutral-500 md:text-sm">
-                    {description}
-                  </Drawer.Description>
-                )}
+          <div className="relative m-0 flex h-full flex-1 gap-2 rounded-l-lg bg-white p-2 py-4 md:m-2 lg:rounded-lg">
+            <div className="my-auto ml-1 h-[100px] w-2 rounded-full bg-muted" />
+            <ScrollArea className="h-[94dvh] w-full">
+              <div className="mx-auto w-full pl-1 pr-3">
+                <div className="mb-4">
+                  <Drawer.Title className="inline-flex w-full items-center gap-2 text-sm font-medium md:text-base">
+                    {title}
+                  </Drawer.Title>
+                  {description && (
+                    <Drawer.Description className="text-xs text-neutral-500 md:text-sm">
+                      {description}
+                    </Drawer.Description>
+                  )}
+                </div>
+                {children}
               </div>
-              {children}
-            </div>
+            </ScrollArea>
           </div>
         </Drawer.Content>
       </Drawer.Portal>
